8304a2f76bd16fb81d12cfde153d8a3aca2a95dc
MarcelaMonteiro <marcelapmont123@gmail.com>
2024-10-10 21:48:36 -0300

    Criação dos endpoint de cadastro e autenticação de usuário. Implementação da página de cadastro de usuários no Blazor integrando com API.

--- FinancasApp.WEB/Models/CriarUsuarioModel.cs
+++ FinancasApp.WEB/Models/CriarUsuarioModel.cs
@@ -1,28 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.Contracts;
-using System.Runtime.InteropServices;
 
 namespace FinancasApp.WEB.Models
 {
+    /// <summary>
+    /// Modelo de dados para capturar o formulário de cadastro de usuário.
+    /// </summary>
     public class CriarUsuarioModel
     {
-        
-[MinLength(8, ErrorMessage = "Por favor, informe no mínimo {1} caracteres.")]
+        [MinLength(8, ErrorMessage = "Por favor, informe no mínimo {1} caracteres.")]
+        [Required(ErrorMessage = "Por favor, informe o nome do usuário.")]
+        public string? Nome { get; set; }
 
-[Required(ErrorMessage = "Por favor, informe o nome do usuário.")]
-    public string? Nome { get; set; }
-    [EmailAddress(ErrorMessage = "Por favor, informe um endereço de email válido.")]
+        [EmailAddress(ErrorMessage = "Por favor, informe um endereço de email válido.")]
+        [Required(ErrorMessage = "Por favor, informe o email do usuário.")]
+        public string? Email { get; set; }
 
-    [Required(ErrorMessage = "Por favor, informe o email do usuário.")]
-    public string? Email { get; set; }
-    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d) (?=.*[\W_]).{8,}$", ErrorMessage = "Por favor, informe a senha com letras minúsculas, maiúsculas, números, símbolos e no mínimo 8 caracteres.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$",
+            ErrorMessage = "Por favor, informe a senha com letras minúsculas, maiúsculas, números, símbolos e no mínimo 8 caracteres.")]
+        [Required(ErrorMessage = "Por favor, informe a senha do usuário.")]
+        public string? Senha { get; set; }
 
-    [Required(ErrorMessage = "Por favor, informe a senha do usuário.")]
-    public string? Senha { get; set; }
-    [Compare("Senha", ErrorMessage = "Senhas não conferem, por favor verifique.")]
-
-    [Required(ErrorMessage = "Por favor, confirme a sua senha.")]
-    public string? SenhaConfirmacao { get; set; }
-        }
+        [Compare("Senha", ErrorMessage = "Senhas não conferem, por favor verifique.")]
+        [Required(ErrorMessage = "Por favor, confirme a sua senha.")]
+        public string? SenhaConfirmacao { get; set; }
     }
+}
+
+
 
